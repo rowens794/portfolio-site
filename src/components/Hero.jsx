4742842d18c77e318e79e844171583e3b537cc1f
rowens794 <rowens794@gmail.com>
2022-09-16 14:29:28 -0400

    added anonymous branch

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,10 +17,10 @@ export function Hero() {
     <div className="flex flex-col-reverse gap-12 lg:flex-row">
       <div className="m-auto h-full max-w-2xl">
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Ryan Owens
+          Ryan XXXXXXXX
         </h1>
         <span className="text-light text-zinc-500">
-          (304) 231-4728 | rowens794@gmail.com
+          (XXX) XXX-XXXX | XXXXXXXXX@gmail.com
         </span>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           I spent a decade in banking until I discovered that I love building
@@ -36,7 +36,7 @@ export function Hero() {
             target="_blank"
           />
           <SocialLink
-            href="https://www.linkedin.com/in/ryan-james-owens/"
+            href="https://www.linkedin.com/"
             aria-label="Follow on LinkedIn"
             icon={LinkedInIcon}
             target="_blank"
